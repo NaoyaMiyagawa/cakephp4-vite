e9b32127871e753a2654f88f60a6a85dd48efc9a
NaoyaMiyagawa <tsuna2764@gmail.com>
2022-02-25 02:03:28 +0900

    feat(tailwind): add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+        // "./webroot/**/*.{js,jsx,ts,tsx,vue}",
+        "./webroot/**/*.{js,jsx,ts,tsx,vue}",
+        "./resources/**/*.{js,jsx,ts,tsx,vue}",
+        "./templates/**/*.php",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
