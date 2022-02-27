afad70d39ffd92652b0f779af2a462353dc3ed94
NaoyaMiyagawa <tsuna2764@gmail.com>
2022-02-27 22:27:21 +0900

    feat(scss): add sass

--- templates/element/hoge.php
+++ templates/element/hoge.php
@@ -6,5 +6,5 @@
 ?>
 
 <div class="hoge">
-    <button class="w-32 p-4 bg-blue-600 border rounded-full text-lime-50 hover:-translate-y-1 active:translate-y-0">Press</button>
+    <button>Press</button>
 </div>
